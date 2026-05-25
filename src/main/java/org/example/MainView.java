64514f97cc20791c59888a4fa41cb96fdcd39d6f
manuujrodcruz <mrmanueljoserodriguezcruz@gmail.com>
2026-05-25 19:31:33 -0400

    initial commit

---
+++ src/main/java/org/example/MainView.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+
+@Route
+public class MainView extends VerticalLayout {
+
+    public MainView() {
+        Button button = new Button("Click me",
+                event -> add(new Paragraph("Clicked!")));
+
+        add(button);
+    }
+}
