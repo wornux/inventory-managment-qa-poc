11519dc152ace66379d19b1521a7208cc5baa94b
Cristian de la Hoz <cyrondelahoz@gmail.com>
2026-06-16 09:37:37 -0400

    chore(mvn): let sprint boom manage versions

--- src/test/java/com/wornux/usecases/PostgresContainerConfig.java
+++ src/test/java/com/wornux/usecases/PostgresContainerConfig.java
@@ -3,14 +3,14 @@ package com.wornux.usecases;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
-import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.postgresql.PostgreSQLContainer;
 
 @TestConfiguration(proxyBeanMethods = false)
 public class PostgresContainerConfig {
 
     @Bean
     @ServiceConnection
-    PostgreSQLContainer<?> postgresContainer() {
-        return new PostgreSQLContainer<>("postgres:17-alpine");
+    PostgreSQLContainer postgresContainer() {
+        return new PostgreSQLContainer("postgres:18.1");
     }
 }
